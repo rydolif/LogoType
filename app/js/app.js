bdacb9fdb9f7ccdec60d9e5907a1a0fd7f7af0d7
master <rudolifrudolif@gmail.com>
2020-06-12 20:57:50 +0300

    pravki + header white + video hero

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	var swiper = new Swiper('.hero__slider', {
 		slidesPerView: 1,
 		effect: 'fade',
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
+		// autoplay: {
+		// 	delay: 5000,
+		// 	disableOnInteraction: false,
+		// },
 		pagination: {
 			el: '.hero__pagination',
 			clickable: true,
@@ -256,7 +256,78 @@ document.addEventListener("DOMContentLoaded", function() {
 
   jQuery.validator.addMethod("phoneno", function(phone_number, element) {
      return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
-  }, "Введите Ваш телефон");
+	}, "Введите Ваш телефон");
 
 
+//------------------------------mask--------------------------
+	var videoEl = document.getElementsByTagName('video')[0],
+			playBtn = document.getElementById('playBtn'),
+			vidControls = document.getElementById('controls'),
+			volumeControl = document.getElementById('volume'),
+			timePicker = document.getElementById('timer');
+		
+	// если браузер может воспроизводить видео удаляем класс
+	videoEl.addEventListener('canplaythrough', function () {
+			vidControls.classList.remove('hidden');
+			videoEl.volume = volumeControl.value;
+	}, false);
+	// запускам или останавливаем воспроизведение
+	playBtn.addEventListener('click', function () {
+			if (videoEl.paused) {
+					videoEl.play();
+			} else {
+					videoEl.pause();
+			}
+	}, false);
+		
+	videoEl.addEventListener('play', function () {
+			playBtn.innerText = "❚❚";
+	}, false);
+		
+	videoEl.addEventListener('pause', function () {
+			playBtn.innerText = "►";
+	}, false);
+		
+	volumeControl.addEventListener('input', function () {
+		
+			videoEl.volume = volumeControl.value;
+	}, false);
+		
+	videoEl.addEventListener('ended', function () {
+			videoEl.currentTime = 0;
+	}, false);
+		
+	videoEl.addEventListener('timeupdate', function () {
+			timePicker.innerHTML = secondsToTime(videoEl.currentTime);
+	}, false);
+		
+	// рассчет отображаемого времени
+	function secondsToTime(time){
+				
+			var h = Math.floor(time / (60 * 60)),
+					dm = time % (60 * 60),
+					m = Math.floor(dm / 60),
+					ds = dm % 60,
+					s = Math.ceil(ds);
+			if (s === 60) {
+					s = 0;
+					m = m + 1;
+			}
+			if (s < 10) {
+					s = '0' + s;
+			}
+			if (m === 60) {
+					m = 0;
+					h = h + 1;
+			}
+			if (m < 10) {
+					m = '0' + m;
+			}
+			if (h === 0) {
+					fulltime = m + ':' + s;
+			} else {
+					fulltime = h + ':' + m + ':' + s;
+			}
+			return fulltime;
+	}
 });
