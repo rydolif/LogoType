1ac2922633d686e1a5059c9b9f1438fd57e88965
rydolif1~ <rudolifrudolif@gmail.com>
2024-03-04 12:12:03 +0200

    debag sass + gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -47,8 +47,8 @@ let paths = {
 // LOGIC
 
 const { src, dest, parallel, series, watch } = require('gulp');
-const sass         = require('gulp-sass');
-const scss         = require('gulp-sass');
+const sass         = require('gulp-sass')(require('sass'));
+const scss         = require('gulp-sass')(require('sass'));
 const less         = require('gulp-less');
 const styl         = require('gulp-stylus');
 const cleancss     = require('gulp-clean-css');
